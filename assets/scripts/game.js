3bbfe7d038d2a785440b1daca4ea03a31533be69
Geoff Nowak <geoffreynowak@gmail.com>
2024-04-22 13:40:04 -0400

    Implemented createBoard function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,13 @@
-const startGame = () => {};
+const startGame = () => {
+  return createBoard();
+};
 
 const stopGame = () => {};
 
 const resetGame = () => {};
 
-const createBoard = () => {};
+const createBoard = () => {
+  const width = 10;
+  const height = 10;
+  const board = Array.from({ length: height }, () => Array(width).fill(0));
+};
